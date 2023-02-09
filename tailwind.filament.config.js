1ed6aa5747cf589353929cc488e901e92b777404
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2023-02-09 11:12:50 +0100

    feat: load google fonts locally

---
+++ tailwind.filament.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+
+    darkMode: 'class',
+
+    theme: {
+        extend: {
+            colors: { 
+                danger: colors.rose,
+                primary: colors.amber,
+                success: colors.green,
+                warning: colors.amber,
+            }, 
+            fontFamily: {
+                sans: ['DM Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography')
+    ],
+};
